6a8a2a5cb963fc67ff46b12430c2deeb607677d8
marc <marc.jermann@fhnw.ch>
2024-05-03 11:06:06 +0200

    refactor: Remove unused import and function call in tf_SLICER_BIDS.py.

--- ETL/Transform/tf_SLICER_BIDS.py
+++ ETL/Transform/tf_SLICER_BIDS.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__))))))
 
-from PyUtilities import read_config_file, mkdir_if_not_exists
+from PyUtilities import read_config_file
 import logging
 import pandas as pd
 import os
